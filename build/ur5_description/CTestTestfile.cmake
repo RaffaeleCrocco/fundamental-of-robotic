5927535f910b088480e57466867847c5de92b8a8
Tobic0 <95741642+Tobic0@users.noreply.github.com>
2021-12-09 09:56:35 +0100

    Delete build directory

--- build/ur5_description/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raptor/robotica/src/ur5/ur5_description
-# Build directory: /home/raptor/robotica/build/ur5_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/ur5_gazebo/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raptor/robotica/src/ur5/ur5_gazebo
-# Build directory: /home/raptor/robotica/build/ur5_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
