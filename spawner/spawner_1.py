d30d47334ccc4d18de3dc6f9563c6256b4cd33b5
Tobic0 <95741642+Tobic0@users.noreply.github.com>
2022-01-02 14:22:52 +0100

    It now spawns random legos

--- spawner/spawner_1.py
+++ spawner/spawner_1.py
@@ -6,9 +6,11 @@ import random
 
 pos = Pose(Point(random.uniform(0.3, 0.8), random.uniform(-0.45, 0.45), 0.775), Quaternion(0,0,random.uniform(-2.0, 2.0), 0.1))
 print(pos)
+brick = "lego_"+str(random.randint(1,11))
+print(brick)
 spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
 spawn_model_client(model_name='test'+str(random.random()), 
-    model_xml=open('src/ur5/ur5_gazebo/models/lego_1/model.sdf', 'r').read(),
+    model_xml=open('src/ur5/ur5_gazebo/models/'+brick+'/model.sdf', 'r').read(),
     robot_namespace='/foo',
     initial_pose=pos,
     reference_frame='world')
