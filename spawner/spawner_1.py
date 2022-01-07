3ff86c51c4afed6043e350d4adb9598d9e69e031
Tobias Carcereri <95741642+Tobic0@users.noreply.github.com>
2022-01-07 23:12:38 +0100

    Update spawner_1.py

--- spawner/spawner_1.py
+++ spawner/spawner_1.py
@@ -4,9 +4,11 @@ from geometry_msgs.msg import *
 import rospy
 import random
 
-pos = Pose(Point(random.uniform(0.3, 0.76), random.uniform(-0.45, 0.45), 0.775), Quaternion(0,0,random.uniform(-2.0, 2.0), 0.0))
+blocks = ['X1-Y1-Z2', 'X1-Y2-Z1', 'X1-Y2-Z2', 'X1-Y2-Z2-CHAMFER', 'X1-Y2-Z2-TWINFILLET', 'X1-Y3-Z2', 'X1-Y3-Z2-FILLET', 'X1-Y4-Z1', 'X1-Y4-Z2', 'X2-Y2-Z2', 'X2-Y2-Z2-FILLET']
+
+pos = Pose(Point(random.uniform(0.3, 0.76), random.uniform(-0.42, 0.42), 0.775), Quaternion(0,0,random.uniform(-2.0, 2.0), 0.0))
 print(pos)
-brick = "lego_"+str(random.randint(1,11))
+brick=blocks[random.randint(0,10)]
 print(brick)
 spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
 spawn_model_client(model_name='test'+str(random.random()), 
