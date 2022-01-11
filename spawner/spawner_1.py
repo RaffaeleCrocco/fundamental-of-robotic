dd2fcd380e404c5ff4f779f9943c880afe8566cd
Tobias Carcereri <95741642+Tobic0@users.noreply.github.com>
2022-01-11 18:10:45 +0100

    Update spawner_1.py

--- spawner/spawner_1.py
+++ spawner/spawner_1.py
@@ -4,12 +4,16 @@ from geometry_msgs.msg import *
 import rospy
 import random
 
+#Array containing all lego blocks names
 blocks = ['X1-Y1-Z2', 'X1-Y2-Z1', 'X1-Y2-Z2', 'X1-Y2-Z2-CHAMFER', 'X1-Y2-Z2-TWINFILLET', 'X1-Y3-Z2', 'X1-Y3-Z2-FILLET', 'X1-Y4-Z1', 'X1-Y4-Z2', 'X2-Y2-Z2', 'X2-Y2-Z2-FILLET']
 
-pos = Pose(Point(random.uniform(0.3, 0.76), random.uniform(-0.42, 0.42), 0.775), Quaternion(0,0,random.uniform(-2.0, 2.0), 0.0))
-print(pos)
+#Generate random position
+pos = Pose(Point(random.uniform(0.3, 0.85), random.uniform(-0.3, 0.3), 0.775), Quaternion(0,0,random.uniform(-3.14, 3.14), 0.0))
+#Get a random lego block from all legos
 brick=blocks[random.randint(0,10)]
+print(pos)
 print(brick)
+#Call rospy spawn function to spawn objects in gazebo
 spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
 spawn_model_client(model_name='test'+str(random.random()), 
     model_xml=open('src/ur5/ur5_gazebo/models/'+brick+'/model.sdf', 'r').read(),
